0bd664f37d07724ddce48d18ce2e49410d9e070f
Saulo Roberto <sauloroberto1000@hotmail.com>
2022-11-13 18:01:41 -0300

    continuando

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <time.h>
+#include <math.h>
 
 #ifndef _CLOCK_TIMER_H
 #define _CLOCK_TIMER_H
@@ -41,17 +42,16 @@ float* lePontosBinario(char *arquivo, long int* qtde_pontos, int* dim_pontos){
        fprintf(stderr, "Erro de leitura das dimensoes da matriz arquivo \n");
        return NULL;
     }
+
     // le dimensão dos pontos
     ret = fread(dim_pontos, sizeof(int), 1, descritorArquivo);
     if(!ret) {
        fprintf(stderr, "Erro de leitura das dimensoes da matriz arquivo \n");
        return NULL;
     }
-    qtde_numeros = (*qtde_pontos) * (*dim_pontos); //calcula a qtde de elementos da matriz
 
-    /*printf("qtde pontos = %ld\n", *qtde_pontos);
-    printf("dim pontos = %d\n", *dim_pontos);
-    printf("qtde numeros = %lld\n", qtde_numeros);*/
+    // quantidade de números
+    qtde_numeros = (*qtde_pontos) * (*dim_pontos); //calcula a qtde de elementos da matriz
 
     //aloca espaço na memória
     pontos = (float*) malloc(sizeof(float) * qtde_numeros);
@@ -67,8 +67,8 @@ float* lePontosBinario(char *arquivo, long int* qtde_pontos, int* dim_pontos){
        return NULL;
     }
 
-    fprintf(stdout, "Leitura dos Pontos:\n");
-    //imprime a matriz na saida padrao
+    // imprime os pontos
+    fprintf(stdout, "Pontos:\n");
     for(i = 0; i < qtde_numeros; i += (*dim_pontos)) {
         for(j = i; j < i + (*dim_pontos); j++){
             fprintf(stdout, "%.6f ", pontos[j]);
@@ -80,65 +80,130 @@ float* lePontosBinario(char *arquivo, long int* qtde_pontos, int* dim_pontos){
     return pontos;
 }
 
+int centroide_mais_proximo(int i_comeco, int dim_pontos, int qtde_centroides, float* centroides, float* pontos){
+    double menor_somatorio = INFINITY;
+    int menor_centroide = 0;
+    for(int j = 0; j < qtde_centroides; j++){
+        float somatorio = 0;
+        for(int k = i_comeco, l = j * dim_pontos; k < i_comeco + dim_pontos, l < (j + 1) * dim_pontos; k++, l++){
+            somatorio += pow(pontos[k] - centroides[l], 2);
+        }
+        somatorio = pow(somatorio, 1.0/2.0);
+        if (somatorio < menor_somatorio){
+            menor_somatorio = somatorio;
+            menor_centroide = j;
+        }
+    }
+
+    return menor_centroide;
+}
+
 int main(int argc, char* argv[]){
 
+    // validando entrada
     if(argc < 4){
         fprintf(stderr, "Digite: %s <arquivo entrada> <qtde clusters> <qtde threads>\n", argv[0]);
         return 1;
     }
 
+    // iteração
     long int i;
-    int j;
+    int j, k;
 
-    int achou;
+    // nome do arquivo que contém os pontos
     char* arquivo = argv[1];
+    // array dos pontos
     float* pontos;
-    float* clusters;
+    int qtde_clusters = atoi(argv[2]);
+    // quantidade de threads
+    int qtde_threads = atoi(argv[3]);
+
     long int qtde_pontos;
     int dim_pontos;
     long long int qtde_numeros;
-    int qtde_clusters = atoi(argv[2]);
-    int qtde_threads = atoi(argv[3]);
+    // quantidade de clusters
 
     pontos = lePontosBinario(arquivo, &qtde_pontos, &dim_pontos);
     if (pontos == NULL)
         return 2;
-    
+
     qtde_numeros = qtde_pontos * dim_pontos;
 
-    /*
-    printf("qtde_pontos = %ld\n", qtde_pontos);
-    printf("dim_pontos = %d\n", dim_pontos);
-    printf("qtde_numeros = %lld\n", qtde_numeros);
-    */
-
-    // gerando os centróides
     srand(time(NULL));
-    int cluster_ids[qtde_clusters]; // lista de índices dos centróides
-    for(i = 0; i < qtde_clusters; i++){
-        int i_ponto;
+    // registra se índice é repetido
+    int achou;
+    // lista de índices dos centróides
+    int centers_indices[qtde_clusters];
+    for(j = 0; j < qtde_clusters; j++){
+        // índice do ponto
+        int indice;
 		do{
 			achou = 0;
-            i_ponto = rand() % qtde_pontos;
-			for(j = 0; j <= i; j++){
-				if(i_ponto == cluster_ids[j]){
+            indice = rand() % qtde_pontos; // gera um índice aleatório
+			for(k = 0; k <= j; k++){ // verifica se índice existe
+				if(indice == centers_indices[k]){ // índice existe
 					achou = 1;
 					break;
 				}
 			}
-		}while(achou);
-		cluster_ids[i] = i_ponto;
+		}while(achou); // caso índice já exista, gera um novo
+		centers_indices[j] = indice; // adiciona o novo íncide no array
 	}
 
-    /*
-    for(i = 0; i < qtde_clusters; i++)
-        printf("cl = %d\n", cluster_ids[i]);
-    */
+    // array de centroides
+    float* centers = malloc(sizeof(float) * qtde_clusters * dim_pontos);
+    // preenche o array de centróides de acordo com os seus índices
+    for(j = 0; j < qtde_clusters; j++){
+        int indice = centers_indices[j]; // índice do array de índices de pontos
+        int indice_comeco = indice * dim_pontos; // ínndice de ínicio
 
-    for(i = 0; i < qtde_clusters; i++){
-        for(j = 0; j < dim_pontos; j++)
-            clusters[i * dim_pontos + j] = pontos[i * dim_pontos + j];
+        for(k = 0, i = indice_comeco; i < indice_comeco + dim_pontos; k++, i++)
+            centers[j * dim_pontos + k] = pontos[i];
     }
 
-    
+    while(1){
+        // array onde cada inteiro representa o centróide mais próximo do ponto de mesma posição
+        int* pontos_centroides = malloc(sizeof(int) * qtde_pontos);
+        for(i = 0; i < qtde_pontos; i++){
+            int menor_centroide = centroide_mais_proximo(i * dim_pontos, dim_pontos, qtde_clusters, centers, pontos);
+            printf("ponto[%ld] = centroide[%d]\n", i, menor_centroide);
+            pontos_centroides[i] = menor_centroide;
+        }
+
+
+        // array dos novos centróides
+        float* new_centers = (float*) calloc(qtde_clusters * dim_pontos, sizeof(float));
+        // array de quantidade de pontos por centróide
+        int* qtde_pontos_centroide = (int*) calloc(qtde_clusters, sizeof(int));
+
+        // gerando a soma e quantidade de pontos por centróide
+        for(i = 0; i < qtde_pontos; i++){
+            int centroide_proximo = pontos_centroides[i];
+            qtde_pontos_centroide[centroide_proximo]++;
+            for(j = centroide_proximo * dim_pontos, k = i * dim_pontos; j < (centroide_proximo + 1) * dim_pontos, k < (i + 1) * dim_pontos; j++, k++){
+                new_centers[j] += pontos[k];
+            }
+        }
+        // dividindo a soma pela quantidade de pontos
+        for(i = 0; i < qtde_clusters; i++){
+            for(j = i * dim_pontos; j < (i + 1) * dim_pontos; j++)
+                new_centers[j] = new_centers[j] / qtde_pontos_centroide[i];
+        }
+
+        achou = 0; // registra se encontrou valores diferentes
+        for(i = 0; i < qtde_clusters; i++){
+            if(fabs(new_centers[i] - centers[i]) > 1e-10){
+                achou = 1;
+                break;
+            }
+        }
+
+        if (!achou){
+            break;
+        }
+        break;
+    }
+    free(pontos);
+    free(centers);
+    return 0;
 }
