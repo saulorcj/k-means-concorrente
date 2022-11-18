133e3d0bfcd470ce8a46ae10b70a0bc5aa72a343
joaomng <73210841+joaomng@users.noreply.github.com>
2022-11-18 19:01:26 -0300

    Gera também .txt pros pontos
    Nota: agora é preciso incluir, como último argumento ao rodar o executável, o nome pro arquivo txt que vai ser gerado como saída
    (mas todos os argumentos anteriores estão mantidos)

--- geraPontos.c
+++ geraPontos.c
@@ -5,12 +5,13 @@
 int main(int argc, char *argv[]){
 
     // confere quantidade de argumentos
-    if (argc < 5){
-        fprintf(stderr, "Digite: %s <qtde pontos> <dim pontos> <limite> <arquivo saida>\n", argv[0]);
+    if (argc < 6){
+        fprintf(stderr, "Digite: %s <qtde pontos> <dim pontos> <limite> <arquivo saida bin> <arquivo de saida txt>\n", argv[0]);
         return 1;
     }
 
     // variáveis
+    long long int aux = 0;
     long int qtde_pontos = atoi(argv[1]); // quantidade de pontos
     int dim_pontos = atoi(argv[2]); // dimensão dos pontos
     int limite = atoi(argv[3]); // valor máximo para uma coordenada
@@ -18,6 +19,7 @@ int main(int argc, char *argv[]){
 
     float* pontos;
     FILE* descritorArquivo; //descritor do arquivo de saida
+    FILE* descritorArquivoTxt;//descritor do arquivo de saída em txt
     size_t ret; //retorno da funcao de escrita no arquivo de saida
 
     /* DEBUG
@@ -35,22 +37,44 @@ int main(int argc, char *argv[]){
 
     // preenchendo os pontos
     srand(time(NULL));
-    for(long int i = 0; i < qtde_numeros; i++)
+    for(long long int i = 0; i < qtde_numeros; i++)
         *(pontos + i) = (rand() % limite);
     
     // abrindo o arquivo
     descritorArquivo = fopen(argv[4], "wb");
-    if(!descritorArquivo) {
+    descritorArquivoTxt = fopen(argv[5], "w");
+    if((!descritorArquivo) || (!descritorArquivoTxt)) {
        fprintf(stderr, "Erro de abertura do arquivo\n");
        return 3;
     }
 
     // escreve quantidade de pontos
     ret = fwrite(&qtde_pontos, sizeof(long int), 1, descritorArquivo);
+    fprintf(descritorArquivoTxt, "%ld\n", qtde_pontos);
     // escreve dimensão dos pontos
     ret = fwrite(&dim_pontos, sizeof(int), 1, descritorArquivo);
+    fprintf(descritorArquivoTxt, "%d\n", dim_pontos);
     // escreve os pontos
     ret = fwrite(pontos, sizeof(float), qtde_numeros, descritorArquivo);
+    for(long long int i=0; i<qtde_numeros;i+=dim_pontos){//vou escrever um ponto por linha
+    
+        fprintf(descritorArquivoTxt, "%f", pontos[i]);
+        aux =1; //aux +=1, mas aux é sempre 0 aqui
+        while(aux<dim_pontos){
+            fprintf(descritorArquivoTxt, " %f", pontos[i+aux]);
+            aux++;
+        }
+        aux=0;
+        fprintf(descritorArquivoTxt, "\n");
+
+    }
+    // a "formatação" do txt é:
+    //qtde de pontos\n
+    //dimensão dos pontos\n
+    //x y z ... \n 
+    //x y z ...
+    //...
+
     if(ret < qtde_numeros) {
        fprintf(stderr, "Erro de escrita no  arquivo\n");
        return 4;
@@ -58,6 +82,7 @@ int main(int argc, char *argv[]){
 
     //finaliza o uso das variaveis
     fclose(descritorArquivo);
+    fclose(descritorArquivoTxt);
     free(pontos);
     return 0;
 }
